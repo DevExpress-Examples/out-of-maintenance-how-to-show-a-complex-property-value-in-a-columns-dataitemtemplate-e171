a43f7ded47db8b216de707fb743fe07d48d074e9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to show a complex property value in a column's DataItemTemplate
+
+
+<p>This example demonstrates how to show a complex property value in a column's DataItemTemplate using BindingExpressions</p><p><strong>See also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/E3593">ASPxGridView - How to use GridViewDataComboBoxColumn with complex properties </a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,26 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxTabControl
+Imports DevExpress.Xpo
+Imports System.Drawing
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+Imports System.Collections.Generic
+Imports DevExpress.Data
+Imports DevExpress.Web.ASPxDataView
+
+Partial Public Class Complex_Property_In_Template
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+	End Sub
+End Class
+
+
